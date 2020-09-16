a8f5c9005826be7e5262047e972c47df5cc99f7d
Arghya Biswas <arghyabiswas2000@gmail.com>
2020-09-16 12:23:27 +0530

    Updated urls

--- slr/slr/urls.py
+++ slr/slr/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from submission import views
 
 
 urlpatterns = [
-    path('', include('basic_app.urls')),
-    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('admin/', admin.site.urls)
 ]
